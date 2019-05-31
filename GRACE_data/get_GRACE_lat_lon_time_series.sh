b615a0f75e30785fd16d7a300bf8e73cb291e7d1
kvngvikram <happykvng@gmail.com>
2019-05-31 11:36:46 +1000

    code to get time series of grace at gps station done

--- GRACE_data/get_GRACE_lat_lon_time_series.sh
+++ GRACE_data/get_GRACE_lat_lon_time_series.sh
@@ -5,41 +5,47 @@
 #           https://www.geeksforgeeks.org/write-bash-script-print-particular-line-file/
 #           http://evc-cit.info/cit052/pass_to_awk.html
 
-#station_file="station_data.csv"
-#while IFS= read -r station
-#do
-#  stringarray=($station)
-# $station is a string with spaces which needs to be split to station code lat and lon
-
-string="WARA -25.037 128.296"
-stringarray=($string)
-name=${stringarray[0]}
-lat=${stringarray[1]}
-lon=${stringarray[2]}
-echo $name 
-echo $lat
-echo $lon
-
+station_file="station_data.csv"
 grace_names_file="names.txt"
 grace_times_file="time_decimal_years.txt"
 
-string="$( wc -l $grace_names_file )"
-stringarray=($string)
-no_files=${stringarray[0]}    # number of files
+rm GRACE_data_*  2> /dev/null   # removing any folder with same name with no error messages 
 
-#for i in $(seq 1 1)
-for i in $(seq 1 $no_files)
+while IFS= read -r string
 do
-  echo $i
-  shc_name="$(awk -v awk_i=$i 'NR==awk_i' $grace_names_file)"
-  shc_time="$(awk -v awk_i=$i 'NR==awk_i' $grace_times_file)"
-  echo $shc_name $shc_time
-  temp_file='temp.dat'
-  echo $lat $lon
-  ./evaluate_sphrarm $shc_name $temp_file  $lat    -99      $lon     -99 10 edefn
-done
 
-
-
+  # $string has spaces which needs to be split to station code lat and lon
+  # example as follows
+  #string="WARA -25.037 128.296"
   
-#done < "$station_file"
+  stringarray=($string)
+  name=${stringarray[0]}
+  lat=${stringarray[1]}
+  lon=${stringarray[2]}
+  
+  #echo $name 
+  #echo $lat
+  #echo $lon
+
+  string="$( wc -l $grace_names_file )"
+  stringarray=($string)
+  no_files=${stringarray[0]}    # number of files
+
+  #for i in $(seq 1 1)
+  for i in $(seq 1 $no_files)
+  do
+
+    shc_name="$(awk -v awk_i=$i 'NR==awk_i' $grace_names_file)"
+    shc_time="$(awk -v awk_i=$i 'NR==awk_i' $grace_times_file)"
+    #echo $shc_name $shc_time
+    temp_file='temp.dat'
+    #echo $lat $lon
+    ./evaluate_sphrarm $shc_name $temp_file  $lat    -99      $lon     -99 10 edefn > /dev/null
+    #echo $shc_time $(cat temp.dat) >> "$(pwd)/$grace_save_folder_name/$name.txt"
+    echo $shc_time $(cat temp.dat) >> "GRACE_data_$name.txt"
+    
+    #echo $name $i
+
+  done
+  echo $name
+done < "$station_file"

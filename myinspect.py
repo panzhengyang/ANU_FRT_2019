7ef7f56acf54c9b143e6dbdecd94860a8d05d423
kvngvikram <happykvng@gmail.com>
2019-06-05 15:54:12 +1000

    got results

--- myinspect.py
+++ myinspect.py
@@ -1,8 +1,17 @@
 import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
 import numpy as np
+import pandas as pd 
+import matplotlib.image as mpimg
 
-plt.figure('test')
-plt.Basemap(width=12000000,height=9000000,projection='lcc',
-            resolution='c',lat_1=45.,lat_2=55,lat_0=50,lon_0=-107.)
+data = pd.read_csv('result.txt',sep='\s+')
+
+np_data = np.asarray(data.iloc[:,1:],dtype=float)
+station = np.asarray(data.iloc[:,0])
+print(station)
+print(np.shape(np_data))
+
+lat , lon , dp , rms_gps , rms_grace , rms_gps_grace , cc , em = np_data[:,0].astype(float) , np_data[:,1].astype(float) , np_data[:,2].astype(float) , np_data[:,3].astype(float) , np_data[:,4].astype(float) , np_data[:,5].astype(float) , np_data[:,6].astype(float) , np_data[:,7].astype(float) 
+
+image = mpimg.imread("mymap.png")
+plt.imshow(image,extent=[-180,180,90,-90])
 plt.show()

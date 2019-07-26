a055d2367fad93bff96b813856b79ae29ec4b34f
Vikram <vikram@conrad.anu.edu.au>
2019-07-26 15:48:15 +1000

    finally

--- combined_visco_elastic/first.py
+++ combined_visco_elastic/first.py
@@ -5,8 +5,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import pandas as pd
+from pandas import read_csv
+from numpy import cos,sin,sqrt
 import sys
+from scipy.special import lpmv
+from scipy.special import factorial as fact
 
 Tmax = 2011.33333333
 Tmin = 2001.0
@@ -20,128 +23,113 @@ if len(sys.argv)<3:
 grace_data_file_name = "GRACE_data/GRACE_data_"+sys.argv[1]+".txt"
 
 min_data_points = 3
-visco_flag = False
 
-if len(sys.argv) > 3 :
-    # header needs to be removed from this file
-    if ( sys.argv[3] == 'V' ):
-        grace_data_file_name = "GRACE_data/GRACE_visco_data_"+sys.argv[1]+".txt"
-        visco_flag = True
-    else: 
-        grace_data_file_name = sys.argv[3]
-
-grace_data = pd.read_csv(grace_data_file_name,
+grace_data = read_csv(grace_data_file_name,
         delimiter=' ',
         header=None,
         skiprows=0,
         dtype=float)
 grace_data = np.asarray(grace_data)
 
-if(visco_flag):
-    Tgrace , Ugrace , USgrace = grace_data[:,0].astype(float) , grace_data[:,3].astype(float) , grace_data[:,4].astype(float)
-else:
-    # time north north_sigma east east_sigma up up_sigma 
-    Tgrace , Ngrace , NSgrace , Egrace , ESgrace , Ugrace , USgrace = grace_data[:,0].astype(float) , grace_data[:,1].astype(float) , grace_data[:,2].astype(float) , grace_data[:,3].astype(float), grace_data[:,4].astype(float) , grace_data[:,5].astype(float) , grace_data[:,6].astype(float)
+# time north north_sigma east east_sigma up up_sigma 
+Tgrace , Ugrace , USgrace = grace_data[:,0].astype(float) , grace_data[:,5].astype(float) , grace_data[:,6].astype(float)
 
-#print(grace_data.shape)
-'''
-gps_data = np.genfromtxt(gps_data_file_name ,
-        skip_header = 1 ,
-        delimiter = None) 
-# The data has variable number of spaces seperating columns
-# None is the default setting which means any white space is taken as delimiter which include single space, many spaces, tab or many tab etc
-#'''
 
 if sys.argv[2] == 'N' :
     #''' For NGL         
     gps_data_file_name = "NGL_GPS_data/GPS_data_"+sys.argv[1]+".txt"
-    gps_data = pd.read_csv(gps_data_file_name,
+    gps_data = read_csv(gps_data_file_name,
             delimiter='\s+',
             header=None,
             skiprows=1)
     gps_data = np.asarray(gps_data)
     #print(gps_data.shape)
 
-    Tgps , EIgps , EFgps , NIgps , NFgps , UIgps , UFgps , ESgps , NSgps , USgps = gps_data[:,2].astype(float) , gps_data[:,7].astype(float) , gps_data[:,8].astype(float) , gps_data[:,9].astype(float) , gps_data[:,10].astype(float) , gps_data[:,11].astype(float) , gps_data[:,12].astype(float) , gps_data[:,14].astype(float) , gps_data[:,15].astype(float) , gps_data[:,16].astype(float) 
+    Tgps ,  UIgps , UFgps , USgps = gps_data[:,2].astype(float)  , gps_data[:,11].astype(float) , gps_data[:,12].astype(float)  , gps_data[:,16].astype(float) 
 
-    Egps = EIgps.astype(float) + EFgps.astype(float)
-    Ngps = NIgps.astype(float) + NFgps.astype(float)
     Ugps = UIgps.astype(float) + UFgps.astype(float)
 
     # Converting units m to mm
-    Egps , Ngps , Ugps = Egps*1000 , Ngps*1000 , Ugps*1000 
+    Ugps =  Ugps*1000 
     #'''
 
 elif sys.argv[2] == 'M' :
 
     #''' For MIT        
     gps_data_file_name = "MIT_GPS_data/GPS_data_"+sys.argv[1]+".txt"
-    gps_data = pd.read_csv(gps_data_file_name,
+    gps_data = read_csv(gps_data_file_name,
             delimiter='\s+',
             header=None,
             skiprows=0)
     gps_data = np.asarray(gps_data)
     #print(gps_data.shape)
 
-    Tgps , Elon , Nlat , Ugps , ESgps , NSgps , USgps = gps_data[:,0].astype(float) , gps_data[:,18].astype(float) , gps_data[:,17].astype(float) , gps_data[:,19].astype(float) , gps_data[:,24].astype(float) , gps_data[:,23].astype(float) , gps_data[:,25].astype(float)  
+    Tgps ,  Ugps , USgps = gps_data[:,0].astype(float) , gps_data[:,19].astype(float) , gps_data[:,25].astype(float)  
 
-    Earth_radius = 6.3781*10**6     # meters
     # Converting units mm
-    Egps , Ngps , Ugps = Earth_radius*np.sin(Nlat*np.pi/180)*(Elon-Elon[0])*1000 , Earth_radius*((Nlat-Nlat[0])*np.pi/180.)*1000 , Ugps*1000 
+    Ugps = Ugps*1000 
     #'''
 
 # getting the latitude and longitude of the station from station data file 
-df=pd.read_csv('station_data.txt',sep='\s+')
+df=read_csv('station_data.txt',sep='\s+')
 st=df[df['name'].str.match(sys.argv[1])]
 lat = float(st.iloc[0,1])
 lon = float(st.iloc[0,2])
 if lon > 180:
     lon = lon-360.0
 print(lat,lon)
+colat = ( 90-lat )*np.pi/180
+lat = lat*np.pi/180
+lon = lon*np.pi/180
+
+coeff_file_name = sys.argv[3]
+
+############## Reading Load Love numbers
+# This is not done in usual way since the symbol for exponent in Load_Love2_CM.dat file is 'D' rather than 'E'. 'D' cannot be interpretted as exponent in python so the character D is replaced with E for all the columns with data as string. Once replaced, it is converted to float.
+load_love_file_name = 'Load_Love2_CM.dat'
+load_love_pd_dataframe =  read_csv(load_love_file_name, header = None, skiprows = 13, sep = '\s+',
+        dtype = str)
+load_love_pd_dataframe[1] = load_love_pd_dataframe[1].str.replace('D','E')
+load_love_pd_dataframe[2] = load_love_pd_dataframe[2].str.replace('D','E')
+load_love_pd_dataframe[3] = load_love_pd_dataframe[3].str.replace('D','E')
+load_love_data = np.asarray(load_love_pd_dataframe,
+        dtype = float)
+h = load_love_data[:,1]
+k = load_love_data[:,3]
 
 
-'''
-# Selecting the GRACE data which is only in a particular time intervel 
-grace_select_flag = np.logical_and((Tgrace > Tmin),(Tgrace < Tmax))
+############## Function values for visco-elastic and vertical elastic
+radius_of_earth = 6378100.0     # in meters m
+Fv = radius_of_earth*(1.1677 * load_love_data[:,0]  - 0.5233)     # Fv values depends on the degree, first column of load_love_data is degree
+Fe =  radius_of_earth * h/(1+k) # This has inf and nan
+Fe[0] = 0   # in CM frame degree 0 and 1 cannot be calculated  
+Fe[1] = 0
 
-Tgrace = Tgrace[grace_select_flag]
-Ngrace = Ngrace[grace_select_flag]
-NSgrace = NSgrace[grace_select_flag]
-Egrace = Egrace[grace_select_flag]
-ESgrace = ESgrace[grace_select_flag]
-Ugrace = Ugrace[grace_select_flag]
-USgrace = USgrace[grace_select_flag]
-#'''
 
-''' Example line for gps:
+############## Normalised Associated Legendre function
+# refer :   http://mitgcm.org/~mlosch/geoidcookbook/node11.html
+#           https://igortitara.files.wordpress.com/2010/04/handbook-of-math-for-engineers-and-scientists1.pdf
+def nalf(n,m,x):
+    # order degree input
+    return lpmv(m,n,x) * (-1)**m * sqrt( (2-float(m==0)) * (2.0*n + 1) * fact(n-m)/fact(n+m) )
 
--------------------
-COVE 10JUL28 2010.5708 55405 1594 4 -112.8  -3815 -0.41548   4276712 0.86481  1687  0.35637  0.1800 0.00065 0.00067 0.00328  0.05829 -0.56488  0.04405
--------------------
 
-Meaning of columns:
+data = np.asarray( read_csv( coeff_file_name , header = None , skiprows = 0 , sep = '\s+' ) ) 
+n = data[:,0].astype(int)
+m = data[:,1].astype(int)
+C = data[:,2].astype(float)
+S = data[:,3].astype(float)
 
-1.  COVE      station name
-2.  10JUL28   date
-3.  2010.5708 decimal year
-4.  55405     modified Julian day
-5.  1594      GPS week
-6.  4         day of GPS week
-7.  -112.8    longitude (degrees) of reference meridian
-8.  -3815     eastings (m), integer portion (from ref. meridian)
-9.  -0.41548  eastings (m), fractional portion
-10.  4276712  northings (m), integer portion (from equator)
-11.  0.86481  northings (m), fractional portion
-12.  1687     vertical (m), integer portion
-13.  0.35637  vertical (m), fractional portion
-14. 0.1800    antenna height (m) assumed from Rinex header
-15. 0.00065   east sigma (m)
-16. 0.00067   north sigma (m)
-17. 0.00328   vertical sigma (m)
-18.  0.05829  east-north correlation coefficient
-19. -0.56488  east-vertical correlation coefficient
-20.  0.04405  north-vertical correlation coefficient
-'''
+elastic_correction_rate = 0.0
+only_visco_elastic_rate = 0.0
+geoid = 0.0
+visco_correction_rate = 0.0
+for i in range( np.size(n)): 
+    elastic_correction_rate         += nalf( n[i] , m[i] , cos(colat) ) * Fe[n[i]] * ( C[i]*cos(m[i]*lon) + S[i]*sin(m[i]*lon) ) 
+    only_visco_elastic_rate   += nalf( n[i] , m[i] , cos(colat) ) * Fv[n[i]] * ( C[i]*cos(m[i]*lon) + S[i]*sin(m[i]*lon) ) 
+    geoid           += nalf( n[i] , m[i] , cos(colat) ) * radius_of_earth * ( C[i]*cos(m[i]*lon) + S[i]*sin(m[i]*lon) ) 
+    visco_correction_rate  += nalf( n[i] , m[i] , cos(colat) ) * ( Fv[n[i]] - Fe[n[i]] )* ( C[i]*cos(m[i]*lon) + S[i]*sin(m[i]*lon) ) 
+
 
 
 
@@ -158,49 +146,26 @@ BTgrace = Tgrace - np.pad(DTgrace,(1,0),'constant',constant_values = (0,0)) / 2
 
 
 ReTgps = Tgrace
-#ReNgps = Ngrace * 0
-#ReEgps = Egrace * 0
 ReUgps = Ugrace * 0
-#ReNSgps = NSgrace * 0
-#ReESgps = ESgrace * 0
 ReUSgps = USgrace * 0
 data_flag = (Tgrace * 0).astype(bool)
 
 for i in range(np.size(Tgrace)):
-    
     selection_flag = np.logical_and( Tgps <= FTgrace[i] , Tgps > BTgrace[i] ) 
     data_flag[i] = selection_flag.sum()     # gets boolean elements, not integers 
-    
-    #print(selection_flag.sum())
-    #print(data_flag[i])
-
-    #ReNgps[i] = Ngps[ selection_flag ].mean()
-    #ReEgps[i] = Egps[ selection_flag ].mean()
     ReUgps[i] = Ugps[ selection_flag ].mean()
-    
-    #ReNSgps[i] = NSgps[ selection_flag ].mean()
-    #ReESgps[i] = ESgps[ selection_flag ].mean()
     ReUSgps[i] = USgps[ selection_flag ].mean()
 
 
 if data_flag.sum() > min_data_points :
     # selecting data only when both GRACE and GPS are simultaniously available
     ReTgps = ReTgps[data_flag]
-    #ReNgps = ReNgps[data_flag]
-    #ReEgps = ReEgps[data_flag]
     ReUgps = ReUgps[data_flag]
-    #ReNSgps = ReNSgps[data_flag]
-    #ReESgps = ReESgps[data_flag]
     ReUSgps = ReUSgps[data_flag]
 
     ReTgrace = Tgrace[data_flag]
-    #ReNgrace = Ngrace[data_flag]
-    #ReEgrace = Egrace[data_flag]
     ReUgrace = Ugrace[data_flag]
-    #ReNSgrace = NSgrace[data_flag]
-    #ReESgrace = ESgrace[data_flag]
     ReUSgrace = USgrace[data_flag]
-    
     
     # time_range_flag, trf is to select only the samples within the range of (Tmin,Tmax)
     trf = np.logical_and(ReTgrace > Tmin , ReTgrace < Tmax)
@@ -211,16 +176,20 @@ if data_flag.sum() > min_data_points :
     # Removing mean value from GPS data
     # This process should not be done initially as mean should be calculated based on data when GRACE data is not present
     # This needs further inspection
-    #ReNgps = ReNgps - ReNgps[trf].mean()
-    #ReEgps = ReEgps - ReEgps[trf].mean()
     ReUgps = ReUgps - ReUgps[trf].mean()
+
+    only_elastic_grace = ReUgrace - 1000*elastic_correction_rate*ReTgrace
+    only_visco_elastic_grace = 1000*only_visco_elastic_rate*ReTgrace
+    
+    corrected_grace = ReUgrace + 1000*visco_correction_rate*ReTgrace
 
     # Removing mean value from GRACE data
     # This process is already done for the data but the mean is for all the original data and now not all the initial data is being used
     # This needs further inspection
-    #ReNgrace = ReNgrace - ReNgrace[trf].mean()
-    #ReEgrace = ReEgrace - ReEgrace[trf].mean()
     ReUgrace = ReUgrace - ReUgrace[trf].mean()
+    only_elastic_grace = only_elastic_grace - only_elastic_grace[trf].mean()
+    only_visco_elastic_grace = only_visco_elastic_grace - only_visco_elastic_grace[trf].mean()
+    corrected_grace = corrected_grace - corrected_grace[trf].mean()
 
     # Calculating the RMS values
     rmse_gps = np.sqrt(np.mean((ReUgps[trf].mean()-ReUgps[trf])**2))
@@ -243,13 +212,18 @@ if data_flag.sum() > min_data_points :
     print('Slope of GRACE is\t',slope_grace)
     print('Slope of GRACE-GPS is\t',slope_diff)
     print('Corcoef is \t',corr_coeff)
-    
+    print('Only visco-elastic slope: \t',only_visco_elastic_rate*1000)
+    print('Only elastic slope: \t',slope_grace-elastic_correction_rate*1000)
+    print('corrected GRACE slope: \t',slope_grace-visco_correction_rate*1000)
 
     #'''
     plt.figure("GRACE and Resampled GPS vertical")
     plt.scatter(Tgps,Ugps-Ugps_mean,label="GPS",s=0.05,c='b')
     plt.plot(ReTgrace,ReUgrace,label="GRACE",linewidth=2,c='r')
     plt.plot(ReTgps,ReUgps,label="ReGPS",linewidth=2,c='b')
+    plt.plot(ReTgrace,only_elastic_grace,label="elastic_GRACE",linewidth=2,c='y')
+    plt.plot(ReTgrace,only_visco_elastic_grace,label="visco_elastic_GRACE",linewidth=2,c='g')
+    plt.plot(ReTgrace,corrected_grace,label="corrected_GRACE",linewidth=2,c='k')
     plt.title("GRACE and ReGPS at "+sys.argv[1])
     plt.xlabel("year")
     plt.ylabel("Vertical (mm)")
@@ -258,9 +232,9 @@ if data_flag.sum() > min_data_points :
     #'''
     #'''
     plt.figure("Map")
-    img = mpimg.imread('images/map.png')
+    img = mpimg.imread('../images/map.png')
     plt.imshow(img,extent=[-180,180,-90,90])
-    plt.scatter(lon,lat)
+    plt.scatter(lon*180/np.pi,lat*180/np.pi)
     plt.title('Location of station '+sys.argv[1])
     plt.xlabel('Longitude')
     plt.ylabel('Latitude')
